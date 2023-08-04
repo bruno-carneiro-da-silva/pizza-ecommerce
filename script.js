a0cff39d9bdf031c925a1df054c8b377b79bb8db
Brunosilva7 <brunocarneirosilva7@gmail.com>
2023-08-04 16:07:36 -0300

    feature[master] - adding some functionalities to the modal

--- script.js
+++ script.js
@@ -1,44 +1,84 @@
-//functions to select the html element
-const selector = (el) => document.querySelector(el);
-const selectorAll = (el) => document.querySelectorAll(el);
+let modalQtd = 1
 
+//functions to select the html element
+const selector = (el) => document.querySelector(el)
+const selectorAll = (el) => document.querySelectorAll(el)
 
 // surfing through the pizzas
-pizzaJson.map((item, index)=>{
-  let pizzaitem = selector('.models .pizza-item').cloneNode(true);
+pizzaJson.map((item, index) => {
+  let pizzaitem = selector('.models .pizza-item').cloneNode(true)
 
-  pizzaitem.setAttribute('data-key', index);
-  pizzaitem.querySelector('.pizza-item--img img').src = item.img;
-  pizzaitem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-  pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name;
-  pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description;
-  pizzaitem.querySelector('a').addEventListener('click', (e) =>{
-    e.preventDefault();
+  pizzaitem.setAttribute('data-key', index)
+  pizzaitem.querySelector('.pizza-item--img img').src = item.img
+  pizzaitem.querySelector(
+    '.pizza-item--price'
+  ).innerHTML = `R$ ${item.price.toFixed(2)}`
+  pizzaitem.querySelector('.pizza-item--name').innerHTML = item.name
+  pizzaitem.querySelector('.pizza-item--desc').innerHTML = item.description
+  pizzaitem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault()
 
-    fillInfosPizzaModal(e);
-    editEl();
-  });
+    modalQtd = 1
+    fillInfosPizzaModal(e)
+    editElPizza()
+  })
 
   //fullfill the infos on screen
-  selector('.pizza-area').append(pizzaitem);
+  selector('.pizza-area').append(pizzaitem)
+})
 
+const editElPizza = () => {
+  selector('.pizzaInfo--qt').innerHTML = modalQtd;
+  selector('.pizzaWindowArea').style.opacity = 0
+  selector('.pizzaWindowArea').style.display = 'flex'
+
+  setTimeout(() => {
+    selector('.pizzaWindowArea').style.opacity = 1
+  }, 200)
+}
+
+const fillInfosPizzaModal = (e) => {
+  let key = e.target.closest('.pizza-item').getAttribute('data-key')
+  selector('.pizzaBig img').src = pizzaJson[key].img
+  selector('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+  selector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+  selector('.pizzaInfo--actualPrice').innerHTML =
+    pizzaJson[key].price.toFixed(2)
+
+  selector('.pizzaInfo--size.selected').classList.remove('selected')
+  selectorAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    if (sizeIndex == 2) {
+      size.classList.add('selected')
+    }
+    size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+  })
+  selectorAll('.pizzaInfo--size').forEach((size) => {
+    size.addEventListener('click',() => {
+      selector('.pizzaInfo--size.selected').classList.remove('selected')
+      size.classList.add('selected');
+    });
+  })
+}
+
+const closeModalPizza = () => {
+  selector('.pizzaWindowArea').style.opacity = 0
+  setTimeout(() => {
+    selector('.pizzaWindowArea').style.display = 'none'
+  }, 500)
+}
+
+selectorAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+  item.addEventListener('click', closeModalPizza);
 });
 
-const editEl = () => {
-  selector('.pizzaWindowArea').style.opacity = 0;
-  selector('.pizzaWindowArea').style.display = 'flex';
+selector('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+  if(modalQtd > 1){
+  modalQtd--;
+  selector('.pizzaInfo--qt').innerHTML = modalQtd;
+  }
+});
+selector('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+  modalQtd++;
+  selector('.pizzaInfo--qt').innerHTML = modalQtd;
 
-  setTimeout(() =>{
-    selector('.pizzaWindowArea').style.opacity = 1;
-  },200);
-}
-
-const fillInfosPizzaModal = (e) =>{
-  let key = e.target.closest('.pizza-item').getAttribute('data-key');
-  selector('.pizzaBig img').src = pizzaJson[key].img
-  selector('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-  selector('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
-  selector('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price.toFixed(2);
-
-
-}
+});
